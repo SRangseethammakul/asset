6eb671a62311073d0d46392f284f4acf34b26d84
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2022-03-29 22:17:11 +0700

    update

--- controllers/calculateController.js
+++ controllers/calculateController.js
@@ -6,7 +6,6 @@ exports.index = async (req, res, next) => {
     let summaryVaa =
       Number(rai) * raiToVaa + Number(ngan) * nganToVaa + Number(vaa);
     let summaryRai = summaryVaa / raiToVaa;
-    console.log(summaryRai);
     let sumPrice = Number(price) * raiToVaa * summaryRai;
     res.status(200).json(sumPrice);
   } catch (error) {
