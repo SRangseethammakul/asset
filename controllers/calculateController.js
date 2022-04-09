c186d9dcbf00ad143296844610c05a90e6cc4cdd
srangseethammakul <srangseethammakul@gmail.com>
2022-04-09 12:43:59 +0700

    update

--- controllers/calculateController.js
+++ controllers/calculateController.js
@@ -1,13 +1,18 @@
 const raiToVaa = 400;
 const nganToVaa = 100;
+const { setNumberformatWithComma } = require("../functions/SetFormat");
 exports.index = async (req, res, next) => {
   try {
     const { rai, ngan, vaa, price } = req.body;
     let summaryVaa =
       Number(rai) * raiToVaa + Number(ngan) * nganToVaa + Number(vaa);
-    let sumPrice = (Number(price) / raiToVaa) * summaryVaa;
-    let priceAvg = price / raiToVaa;
-    res.status(200).json({ calculatePrice: sumPrice, summaryVaa, priceAvg });
+    let sumPrice = ((Number(price) / raiToVaa) * summaryVaa).toFixed(2);
+    let priceAvg = (price / raiToVaa).toFixed(2);
+    sumPrice = await setNumberformatWithComma(sumPrice);
+    priceAvg = await setNumberformatWithComma(priceAvg);
+    summaryVaa = await setNumberformatWithComma(summaryVaa);
+    pricePerRai = await setNumberformatWithComma(price);
+    res.status(200).json({ calculatePrice: sumPrice, summaryVaa, priceAvg, pricePerRai: pricePerRai });
   } catch (error) {
     next(error);
   }
@@ -19,7 +24,12 @@ exports.calculatePerSize = async (req, res, next) => {
       Number(rai) * raiToVaa + Number(ngan) * nganToVaa + Number(vaa);
     let avgPrice = ((Number(price) / summaryVaa) * raiToVaa).toFixed(2);
     let priceAvg = (avgPrice / raiToVaa).toFixed(2);
-    res.status(200).json({ calculatePrice: avgPrice, summaryVaa, priceAvg });
+    avgPrice = await setNumberformatWithComma(avgPrice);
+    priceAvg = await setNumberformatWithComma(priceAvg);
+    summaryVaa = await setNumberformatWithComma(summaryVaa);
+    newPrice = await setNumberformatWithComma(price);
+
+    res.status(200).json({ calculatePrice: newPrice, summaryVaa, priceAvg, pricePerRai :avgPrice });
   } catch (error) {
     next(error);
   }
---
+++ functions/SetFormat.js
@@ -0,0 +1,12 @@
+const setNumberformatWithComma = async (number) => {
+    return new Promise((resolve, reject) => {
+      try {
+        resolve(number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+  module.exports = {
+    setNumberformatWithComma
+  };
