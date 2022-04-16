c30a2f493b068e65013736835f8696234f508dc2
srangseethammakul <srangseethammakul@gmail.com>
2022-04-16 21:21:21 +0700

    update function calculate

--- controllers/calculateController.js
+++ controllers/calculateController.js
@@ -10,9 +10,7 @@ exports.index = async (req, res, next) => {
     let priceAvg = (price / raiToVaa).toFixed(2);
     sumPrice = await setNumberformatWithComma(sumPrice);
     priceAvg = await setNumberformatWithComma(priceAvg);
-    summaryVaa = await setNumberformatWithComma(
-      parseInt(summaryVaa).toFixed(2)
-    );
+    summaryVaa = await setNumberformatWithComma(summaryVaa.toFixed(2));
     pricePerRai = await setNumberformatWithComma(parseInt(price).toFixed(2));
     res.status(200).json({
       calculatePrice: sumPrice,
@@ -33,9 +31,7 @@ exports.calculatePerSize = async (req, res, next) => {
     let priceAvg = (avgPrice / raiToVaa).toFixed(2);
     avgPrice = await setNumberformatWithComma(avgPrice);
     priceAvg = await setNumberformatWithComma(priceAvg);
-    summaryVaa = await setNumberformatWithComma(
-      parseInt(summaryVaa).toFixed(2)
-    );
+    summaryVaa = await setNumberformatWithComma(summaryVaa.toFixed(2));
     newPrice = await setNumberformatWithComma(parseInt(price).toFixed(2));
 
     res.status(200).json({
@@ -53,8 +49,8 @@ exports.calculatePerVaa = async (req, res, next) => {
     const { rai, ngan, vaa, price } = req.body;
     let summaryVaa =
       Number(rai) * raiToVaa + Number(ngan) * nganToVaa + Number(vaa);
-    summaryVaa = await setNumberformatWithComma(summaryVaa);
     let sumPrice = (summaryVaa * Number(price)).toFixed(2);
+    summaryVaa = await setNumberformatWithComma(summaryVaa);
     sumPrice = await setNumberformatWithComma(sumPrice);
     let pricePerRai = (Number(price) * raiToVaa).toFixed(2);
     pricePerRai = await setNumberformatWithComma(pricePerRai);
